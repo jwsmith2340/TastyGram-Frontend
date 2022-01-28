d3eae82f681899df9de6bff060c1ca384d71fc85
Jared Bradley <jaredbradley@jareds-mbp.lan>
2022-01-28 15:08:30 -0600

    add form to allowing posting

--- src/pages/New.js
+++ src/pages/New.js
@@ -1,5 +1,39 @@
+// IMPORT STATE FROM REACT
+import { useState } from "react";
+
 const New = (props) => {
-    return <h1>New</h1>
+    // STATE TO HOLD FORMDATA
+    const [ newForm, setNewForm ] = useState({
+        title: "",
+        image: "",
+        description: "",
+    });
+
+    // HANDLECHANGE FUNCTION FOR FORM
+    const handleChange = (event) => {
+        setNewForm({...newForm, [event.target.name]: event.target.value});
+    };
+
+    //HANDLESUBMIT FUNCTION FOR FORM
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        props.createFood(newForm);
+        setNewForm({
+            title: "",
+            image: "",
+            description: "",
+        });
+    };
+    return (
+        <section>
+            <form onSubmit={handleSubmit}>
+                <input type="text" value={newForm.title} name="title" placeholder="title" onChange={handleChange}/>
+                <input type="text" value={newForm.image} name="image" placeholder="image URL" onChange={handleChange}/>
+                <input type="text" value={newForm.description} name="description" placeholder="description" onChange={handleChange}/>
+                <input type="submit" value="POST FOOD" />
+            </form>
+        </section>
+    )
 };
 
 export default New;
