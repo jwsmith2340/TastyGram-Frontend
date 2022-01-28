81c451b5d9ec39301bc319bea9ee8e03c580aeec
James Smith <jwsmith2340@yahoo.com>
2022-01-28 13:33:43 -0500

    styling the footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,21 @@
+import './Footer.scss'
+
 const Footer = (props) => {
     return (
-        <>
-        <h4>Jared Bradley <span>&copy;</span></h4>
-        <h4>James Smith <span>&copy;</span></h4>
-        <h4>Micah Stewart <span>&copy;</span></h4>
-        </>
+
+        <footer>
+            <div className="footer-box">
+            <div className="mob-name-box">
+                <h4><span>&copy;</span>Jared Bradley</h4>
+                <h4><span>&copy;</span>James Smith</h4>
+                <h4><span>&copy;</span>Micah Stewart</h4>
+            </div>
+            <div className="mob-repo-box">
+                <a href="https://github.com/Jab7red/Project-3-frontend">Frontend Git Repo</a>    
+            </div>
+            </div>  
+
+        </footer>
     )
 };
 
