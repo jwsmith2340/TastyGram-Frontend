ad7caff1571551b3352beab1ab4b1a39a2f5e3a5
Jared Bradley <jaredbradley@jareds-mbp.lan>
2022-01-27 23:11:41 -0600

    add basic Footer.js data

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,11 @@
 const Footer = (props) => {
-    return <h1>Footer</h1>
+    return (
+        <>
+        <h4>Jared Bradley <span>&copy;</span></h4>
+        <h4>James Smith <span>&copy;</span></h4>
+        <h4>Micah Stewart <span>&copy;</span></h4>
+        </>
+    )
 };
 
 export default Footer;
