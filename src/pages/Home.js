4d6dd7160af7fc9d9e622ad29ccbdc03ad85acb9
James Smith <jwsmith2340@yahoo.com>
2022-01-28 13:39:53 -0500

    reduced size of images for development purposes, linked scss file to Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,13 @@
 // IMPORT LINK
 import { Link } from "react-router-dom";
+import './Home.scss'
 
 const Home = (props) => {
     // Loaded Function
     const loaded = () => {
         return props.foods.map((eachFood) => (
-            <div>
-                <div key={eachFood._id}>
+            <div className="home-box">
+                <div key={eachFood._id} className="home-inner-box">
                     <Link to={`/food/${eachFood._id}`}><img src={eachFood.image} alt={eachFood.title}/></Link>
                     <h3>{eachFood.title}</h3>
                 </div>
