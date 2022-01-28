99076bbadb6ff834c53595f0a3957d7e1c1eb4db
James Smith <jwsmith2340@yahoo.com>
2022-01-28 12:33:53 -0500

    navbar styling in header.js and header.scss

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,15 +3,22 @@ import './Header.scss'
 
 const Header = (props) => {
     return (
-            <nav>
+        <nav>
+            <div className="mob-link-boxes">
                 <Link to="/" className="link">
                     <h4>Home</h4>
                 </Link>
-                <br />
+            </div>
+
+            <h2 className="mob-logo">TG</h2>
+            <h2 className="dt-logo">TastyGram</h2>
+
+            <div className="mob-link-boxes">
                 <Link to="/newfood" className="link">
                     <h4>New Post</h4>
                 </Link>
-            </nav>
+            </div>
+        </nav>
     )
 };
 
